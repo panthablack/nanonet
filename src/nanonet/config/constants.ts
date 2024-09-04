65a7606132bd73778f4261c2d0e607a24b778d43
James Randall <james@manmachineltd.com>
2024-09-04 16:27:08 +1000

    Added Activation functions const

--- src/nanonet/config/constants.ts
+++ src/nanonet/config/constants.ts
@@ -1,3 +1,14 @@
 export const MODES = {
   MULTILAYER_PERCEPTRON: 1,
+  CONVOLUTIONAL_NEURAL_NETWORK: 2,
+  TRANSFORMER: 3,
+} as const
+
+export const ACTIVATION_FUNCTIONS = {
+  SIGMOID: 1,
+  RELU: 2,
+  TAN_H: 3,
+  GELU: 4,
+  SILU: 5,
+  SWISH: 6,
 } as const
